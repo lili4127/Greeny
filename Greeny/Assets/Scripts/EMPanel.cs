be71e68cbf3df7e42a198b00cd34a991665249b2
Lior Tal <62267190+lili4127@users.noreply.github.com>
2022-01-28 17:15:29 -0800

    image instead of line rend

--- Greeny/Assets/Scripts/EMPanel.cs
+++ Greeny/Assets/Scripts/EMPanel.cs
@@ -1,51 +1,39 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class EMPanel : MonoBehaviour
 {
     [SerializeField] private float duration = 5f;
     [SerializeField] private TextMeshProUGUI[] waveNames;
     [SerializeField] private TextMeshProUGUI eMText;
+    [SerializeField] private Image eMImage;
     private Color32[] rainbowColors = new Color32[] {  Color.red, new Color(1, 0.5f, 0, 1), Color.yellow, Color.green, Color.blue, new Color(0.3f, 0, 0.5f, 1), new Color(0.6f, 0, 0.8f, 1) };
     private Color startColor = new Color(1, 1, 1, 0);
 
     private void Awake()
     {
-        ResetTexts();
+        ResetPanel();
     }
 
     private void OnEnable()
     {
+        StartCoroutine(FadeInImage(duration));
         StartCoroutine(FadeInTexts(duration));
     }
 
-    private IEnumerator RainbowTextCo(TextMeshProUGUI t, float duration)
+    IEnumerator FadeInImage(float duration)
     {
-        t.ForceMeshUpdate();
-        TMP_TextInfo textInfo = t.textInfo;
-        int characterCount = textInfo.characterCount;
-        int currentCharacter = 0;
-        bool going = true;
-        Color32[] newVertexColors;
+        float time = 0;
 
-        while (going)
+        while (time < duration)
         {
-            for (int i = 0; i < textInfo.characterCount; i++)
-            {
-                int materialIndex = textInfo.characterInfo[currentCharacter].materialReferenceIndex;
-                newVertexColors = textInfo.meshInfo[materialIndex].colors32;
-                int vertexIndex = textInfo.characterInfo[currentCharacter].vertexIndex;
-                newVertexColors[vertexIndex + 0] = rainbowColors[i];
-                newVertexColors[vertexIndex + 1] = rainbowColors[i];
-                newVertexColors[vertexIndex + 2] = rainbowColors[i];
-                newVertexColors[vertexIndex + 3] = rainbowColors[i];
-                t.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
-                currentCharacter++;
-                yield return new WaitForSeconds(0.15f);
-            }
-            going = false;
+            eMImage.fillAmount = Mathf.Lerp(0, 1, time / duration);
+            time += Time.deltaTime;
+            yield return null;
         }
+        eMImage.fillAmount = 1;
     }
 
     IEnumerator FadeInTexts(float duration)
@@ -80,6 +68,34 @@ public class EMPanel : MonoBehaviour
         StartCoroutine(PulseTextCo(t));
     }
 
+    private IEnumerator RainbowTextCo(TextMeshProUGUI t, float duration)
+    {
+        t.ForceMeshUpdate();
+        TMP_TextInfo textInfo = t.textInfo;
+        int characterCount = textInfo.characterCount;
+        int currentCharacter = 0;
+        bool going = true;
+        Color32[] newVertexColors;
+
+        while (going)
+        {
+            for (int i = 0; i < textInfo.characterCount; i++)
+            {
+                int materialIndex = textInfo.characterInfo[currentCharacter].materialReferenceIndex;
+                newVertexColors = textInfo.meshInfo[materialIndex].colors32;
+                int vertexIndex = textInfo.characterInfo[currentCharacter].vertexIndex;
+                newVertexColors[vertexIndex + 0] = rainbowColors[i];
+                newVertexColors[vertexIndex + 1] = rainbowColors[i];
+                newVertexColors[vertexIndex + 2] = rainbowColors[i];
+                newVertexColors[vertexIndex + 3] = rainbowColors[i];
+                t.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+                currentCharacter++;
+                yield return new WaitForSeconds(0.15f);
+            }
+            going = false;
+        }
+    }
+
     IEnumerator PulseTextCo(TextMeshProUGUI t)
     {
         for (float i = 1f; i <= 1.1f; i += 0.005f)
@@ -104,7 +120,7 @@ public class EMPanel : MonoBehaviour
         t.rectTransform.localScale = new Vector3(1f, 1f, 1f);
     }
 
-    private void ResetTexts()
+    private void ResetPanel()
     {
         foreach (TextMeshProUGUI t in waveNames)
         {
@@ -112,11 +128,12 @@ public class EMPanel : MonoBehaviour
         }
 
         eMText.color = startColor;
+        eMImage.fillAmount = 0;
     }
 
     private void OnDisable()
     {
-        ResetTexts();
+        ResetPanel();
         StopAllCoroutines();
     }
 }
